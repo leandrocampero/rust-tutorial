b7c5a6bae1aeb42999cb34175ac563b0c4800dc8
Leandro Campero <leandrocampero14@gmail.com>
2023-05-21 23:31:28 -0300

    experimenting with numbers

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,33 @@ use std::io;
 use std::io::{BufReader, ErrorKind, Write};
 
 fn main() {
-    let is_true = true;
-    let my_gradee = 'A'; // Double quotes are for strings, single quotes are for characters
+    let num_1: f32 = 1.111111111111111;
+    println!("f32: {}", num_1 + 0.000000000000000222);
+
+    let num_2: f64 = 1.111111111111111;
+    println!("f64: {}", num_2 + 0.000000000000000222);
+
+    /*
+    f32: 1.1111112
+    f64: 1.1111111111111112
+     */
+
+    let num_3: u32 = 5;
+    let num_4: u32 = 4;
+    println!("{} + {} = {}", num_3, num_4, num_3 + num_4,);
+    println!("{} - {} = {}", num_3, num_4, num_3 - num_4,);
+    println!("{} * {} = {}", num_3, num_4, num_3 * num_4,);
+    println!("{} / {} = {}", num_3, num_4, num_3 / num_4,);
+    println!("{} % {} = {}", num_3, num_4, num_3 % num_4,);
+
+    /*
+    5 + 4 = 9
+    5 - 4 = 1
+    5 * 4 = 20
+    5 / 4 = 1
+    5 % 4 = 1
+     */
+
+    let random_num = rand::thread_rng().gen_range(1..101); // Doesn't require changing the seed, like C
+    println!("Random: {}", random_num)
 }

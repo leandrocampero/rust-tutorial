3ab1084598c270ec5a3174f0bc2be8e564fd6f2a
Leandro Campero <leandrocampero14@gmail.com>
2023-05-21 23:11:55 -0300

    basic IO ops

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 #![allow(unused)]
 
 use rand::Rng;
+use std::cmp::Ordering;
+use std::fs::File;
 use std::io;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    println!("Hello, world!");
+    println!("What is your name?");
+
+    let mut name = String::new();
+    let greeting = "Nice to meet you";
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Didn't receive input");
+
+    println!("Hello {}! {}", name.trim_end(), greeting);
 }

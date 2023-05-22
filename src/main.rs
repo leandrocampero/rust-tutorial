009eb5e35d988967ba48a94cd76e06fba496a474
Leandro Campero <leandrocampero14@gmail.com>
2023-05-21 23:19:34 -0300

    max values of some data types

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,10 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    const ONE_MIL: u32 = 1_000_000;
-    const PI: f32 = 3.14159265;
-    let age = "47";
-    let mut age: u32 = age.trim().parse().expect("Age wasn't assigned a number");
-    // shadowing: overload a variable as long as they have different data types
-
-    age = age + 1;
-
-    println!("I'm {} and I want ${}", age, ONE_MIL);
+    println!("Max u32: {}", u32::MAX);
+    println!("Max u64: {}", u64::MAX);
+    println!("Max usize: {}", usize::MAX);
+    println!("Max u128: {}", u128::MAX);
+    println!("Max f32: {}", f32::MAX);
+    println!("Max i64: {}", i64::MAX);
 }

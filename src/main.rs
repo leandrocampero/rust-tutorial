bc8f287719eb308794d8f2fe7ce3de27472141a2
Leandro Campero <leandrocampero14@gmail.com>
2023-05-21 23:23:10 -0300

    string vs characters

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use rand::Rng;
 use std::cmp::Ordering;
 use std::fs::File;
@@ -5,6 +7,6 @@ use std::io;
 use std::io::{BufReader, ErrorKind, Write};
 
 fn main() {
-    let _is_true = true;
-    // Any unused variable will throw error. Unless I type '_' before the name
+    let is_true = true;
+    let my_gradee = 'A'; // Double quotes are for strings, single quotes are for characters
 }

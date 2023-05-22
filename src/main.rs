f81bcc2ce91a89b8e769349f637977451fb159a7
Leandro Campero <leandrocampero14@gmail.com>
2023-05-22 00:23:53 -0300

    tuples (object like) and multiple assingment (destructuring like)

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,10 @@ use std::io::{BufReader, ErrorKind, Write};
 use std::{array, io};
 
 fn main() {
-    let arr_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    println!("1st: {}", arr_1[0]);
-    println!("length: {}", arr_1.len());
+    let my_tuple: (u8, String, f64) = (47, "Derek".to_string(), 50_000.00);
 
-    let mut loop_index = 0;
+    println!("Name: {}", my_tuple.1);
 
-    loop {
-        if arr_1[loop_index] % 2 == 0 {
-            loop_index += 1;
-            continue;
-        }
-        if arr_1[loop_index] == 9 {
-            break;
-        }
-        println!("Val: {}", arr_1[loop_index]);
-        loop_index += 1;
-    }
-
-    loop_index = 0;
-    while loop_index < arr_1.len() {
-        println!("Array with 'while': {}", arr_1[loop_index]);
-        loop_index += 1;
-    }
-
-    loop_index = 0;
-    for val in arr_1.iter() {
-        println!("Array with 'for': {}", val);
-    }
+    let (v1, v2, v3) = my_tuple;
+    println!("Age: {}", v1)
 }

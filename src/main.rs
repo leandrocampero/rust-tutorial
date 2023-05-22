06114e301747f119249f4ddd03ae66230d2b4400
Leandro Campero <leandrocampero14@gmail.com>
2023-05-22 11:02:57 -0300

    hashmaps

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,30 @@
 
 use rand::Rng;
 use std::cmp::Ordering;
+use std::collections::HashMap;
 use std::fmt::Display;
 use std::fs::File;
 use std::io::{BufReader, ErrorKind, Write};
 use std::ops::Add;
 use std::{array, io};
 
-fn print_string(x: String) {
-    println!("A string: {}", x)
-}
-
-fn print_return_string(x: String) -> String {
-    println!("Another string: {}", x);
-    return x;
-}
-
-fn change_string(name: &mut String) {
-    name.push_str(" is happy");
-    println!("Message: {}", name)
-}
-
 fn main() {
-    let str1 = String::from("World");
+    let mut heroes = HashMap::new();
+    heroes.insert("Superman", "Clark Kent");
+    heroes.insert("Batman", "Bruce Wayne");
+    heroes.insert("The Flash", "Barry Allen");
 
-    // // let str2 = str1;
-    // // By doing this, the value of str1 is passed to str2. And now, str1 no longer exists
-    // //println!("Hello {}!", str1) // Error
+    for (k, v) in heroes.iter() {
+        println!("{} = {}", k, v)
+    }
 
-    let str2 = str1.clone();
-    println!("Hello {}!", str1); // Works fine
+    println!("Length: {}", heroes.len());
 
-    let str3 = print_return_string(str1);
-    println!("str3 = {}", str3);
-
-    let mut str4 = String::from("Derek");
-    change_string(&mut str4)
+    if heroes.contains_key(&"Batman") {
+        let the_batman = heroes.get(&"Batman");
+        match the_batman {
+            Some(x) => println!("Batman is a hero"),
+            None => println!("Batman is not a hero"),
+        }
+    }
 }

998d915e06d1f6935a7602dfbcb5bfaff1a1785e
Leandro Campero <leandrocampero14@gmail.com>
2023-05-22 00:21:29 -0300

    loops and arrays

--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,36 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::fs::File;
-use std::io;
 use std::io::{BufReader, ErrorKind, Write};
+use std::{array, io};
 
 fn main() {
-    let age = 8;
+    let arr_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    println!("1st: {}", arr_1[0]);
+    println!("length: {}", arr_1.len());
 
-    if (age >= 1) && (age <= 18) {
-        println!("Important birthday");
-    } else if (age == 21) || (age == 50) {
-        println!("Important birthday");
-    } else if age >= 65 {
-        println!("Important birthday");
-    } else {
-        println!("Not an important birthday");
+    let mut loop_index = 0;
+
+    loop {
+        if arr_1[loop_index] % 2 == 0 {
+            loop_index += 1;
+            continue;
+        }
+        if arr_1[loop_index] == 9 {
+            break;
+        }
+        println!("Val: {}", arr_1[loop_index]);
+        loop_index += 1;
     }
 
-    let mut my_age = 47;
-    let can_vote = if my_age >= 18 { true } else { false };
-    println!("Can vote?: {}", can_vote);
+    loop_index = 0;
+    while loop_index < arr_1.len() {
+        println!("Array with 'while': {}", arr_1[loop_index]);
+        loop_index += 1;
+    }
 
-    let age2 = 8;
-    match age2 {
-        1..=18 => println!("Important birthday"),
-        21 | 50 => println!("Important birthday"),
-        65.. => println!("Important birthday"),
-        _ => println!("Not an important birthday"),
-    };
-
-    let my_age = 18;
-    let voting_age = 18;
-    match my_age.cmp(&voting_age) {
-        Ordering::Less => println!("Can't vote"),
-        Ordering::Greater => println!("Can vote"),
-        Ordering::Equal => println!("You gained the right to vote"),
-    };
+    loop_index = 0;
+    for val in arr_1.iter() {
+        println!("Array with 'for': {}", val);
+    }
 }

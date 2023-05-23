d4ccff333287ba5d9da9d43a9090d6e1f3535e01
Leandro Campero <leandrocampero14@gmail.com>
2023-05-23 09:02:40 -0300

    files and error handling

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::fmt::Display;
 use std::fs::File;
-use std::io::{BufReader, ErrorKind, Write};
+use std::io::{BufRead, BufReader, ErrorKind, Write};
 use std::ops::Add;
 use std::{array, io};
 
@@ -13,11 +13,34 @@ mod restaurant;
 use crate::restaurant::order_food;
 
 fn main() {
-    /**
-     * Crates: Modules that produce a library or executable
-     * Modules: Organize and handle privacy
-     * Packages: Build, test and share crates
-     * Paths: A way of naming an item such as a struct, function
-     */
-    order_food();
+    let path = "lines.txt";
+    let output = File::create(path);
+    let mut output = match output {
+        Ok(file) => file,
+        Err(error) => {
+            // Also works without the curly braces
+            panic!("Problem creating the file: {:?}", error)
+        }
+    };
+
+    write!(output, "Just some text\nHello world!").expect("Failed to write to file");
+
+    let input = File::open(path).unwrap();
+    let buffered = BufReader::new(input);
+
+    for line in buffered.lines() {
+        println!("{}", line.unwrap())
+    }
+
+    let output2 = File::create("rand.txt");
+    let output2 = match output2 {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("rand.txt") {
+                Ok(file_created) => file_created,
+                Err(e) => panic!("Can't create error: {:?}", e),
+            },
+            _other_error => panic!("Problem opening file: {:?}", error),
+        },
+    };
 }

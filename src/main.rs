c82c57e1b8d2ad8379764dd498ad24d346f54615
Leandro Campero <leandrocampero14@gmail.com>
2023-05-23 08:44:40 -0300

    modules and crates

--- src/main.rs
+++ src/main.rs
@@ -9,69 +9,15 @@ use std::io::{BufReader, ErrorKind, Write};
 use std::ops::Add;
 use std::{array, io};
 
+mod restaurant;
+use crate::restaurant::order_food;
+
 fn main() {
-    const PI: f32 = 3.14159265;
-    struct Customer {
-        name: String,
-        address: String,
-        balance: f32,
-    }
-
-    let mut bob = Customer {
-        name: String::from("Bob Smith"),
-        address: String::from("555 main st."),
-        balance: 234.50,
-    };
-
-    bob.address = String::from("505 2nd St");
-
-    trait Shape {
-        // Sounds like an Interface in Object Oriented Languages
-        fn new(length: f32, width: f32) -> Self;
-        fn area(&self) -> f32;
-    }
-
-    struct Rectangle {
-        length: f32,
-        width: f32,
-    }
-
-    struct Circle {
-        length: f32,
-        width: f32,
-    }
-
-    impl Shape for Rectangle {
-        fn new(length: f32, width: f32) -> Rectangle {
-            return Rectangle { length, width };
-        }
-        fn area(&self) -> f32 {
-            return self.length * self.width;
-        }
-    }
-
-    impl Shape for Circle {
-        fn new(length: f32, width: f32) -> Circle {
-            return Circle { length, width };
-        }
-        fn area(&self) -> f32 {
-            return (self.length / 2.0).powf(2.0) * PI;
-        }
-    }
-
-    let rec: Rectangle = Shape::new(10.0, 20.0);
-    let circ: Circle = Shape::new(8.0, 2.0);
-
-    println!(
-        "Rec of {} by {} whose area is {}",
-        rec.length,
-        rec.width,
-        rec.area()
-    );
-    println!(
-        "Circle of {} by {} whose area is {}",
-        circ.length,
-        circ.width,
-        circ.area()
-    )
+    /**
+     * Crates: Modules that produce a library or executable
+     * Modules: Organize and handle privacy
+     * Packages: Build, test and share crates
+     * Paths: A way of naming an item such as a struct, function
+     */
+    order_food();
 }

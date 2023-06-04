69d7f7f99f9e705396b7c86f010c41f5f3b5a495
Leandro Campero <leandrocampero14@gmail.com>
2023-06-04 19:51:55 -0300

    concurrency

--- src/main.rs
+++ src/main.rs
@@ -7,43 +7,25 @@ use std::fmt::Display;
 use std::fs::File;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 use std::ops::Add;
+use std::thread;
+use std::time::Duration;
 use std::{array, io};
 
 mod restaurant;
 use crate::restaurant::order_food;
 
 fn main() {
-    // BOX (smart pointer)
-    let b_int1 = Box::new(10);
-    println!("b_int1 = {}", b_int1);
+    let thread1 = thread::spawn(|| {
+        for i in 1..25 {
+            println!("Spawned thread: {}", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
 
-    struct TreeNode<T> {
-        pub left: Option<Box<TreeNode<T>>>,
-        pub right: Option<Box<TreeNode<T>>>,
-        pub key: T,
+    for i in 1..20 {
+        println!("Main thread: {}", i);
+        thread::sleep(Duration::from_millis(1));
     }
 
-    impl<T> TreeNode<T> {
-        pub fn new(key: T) -> Self {
-            TreeNode {
-                left: None,
-                right: None,
-                key,
-            }
-        }
-
-        pub fn left(mut self, node: TreeNode<T>) -> Self {
-            self.left = Some(Box::new(node));
-            self
-        }
-
-        pub fn right(mut self, node: TreeNode<T>) -> Self {
-            self.right = Some(Box::new(node));
-            self
-        }
-    }
-
-    let node1 = TreeNode::new(1)
-        .left(TreeNode::new(2))
-        .right(TreeNode::new(3));
+    thread1.join().unwrap();
 }
